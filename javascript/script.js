585612bc154e5b6da6b74741360dcc4b02521556
Swastik Kumar <swastiktovisit1234@gmail.com>
2022-07-04 04:43:05 +0530

    v2__basic_portfolio

--- javascript/script.js
+++ javascript/script.js
@@ -1,53 +1,282 @@
-'use strict';
+"use strict";
+// Title tab
+window.onload = function () {
+  let pageTitle = document.title;
+  let attentionMessage = "Come Back 🙏🙏";
+  let blinkEvent = null;
 
+  document.addEventListener("visibilitychange", function (e) {
+    let isPageActive = !document.hidden;
+
+    if (!isPageActive) {
+      blink();
+    } else {
+      document.title = pageTitle;
+      clearInterval(blinkEvent);
+    }
+  });
+
+  function blink() {
+    blinkEvent = setInterval(function () {
+      if (document.title === attentionMessage) {
+        document.title = pageTitle;
+      } else {
+        document.title = attentionMessage;
+      }
+    }, 1000);
+  }
+};
+
+// fade in
+let opacity = 0;
+let intervalID = 0;
+let change_title = window.onload;
+window.onload = function () {
+  fadeIn();
+  if (change_title) {
+    change_title();
+  }
+};
+function fadeIn() {
+  setInterval(show, 110);
+}
+function show() {
+  var body = document.getElementById("body");
+  opacity = Number(window.getComputedStyle(body).getPropertyValue("opacity"));
+  if (opacity < 1) {
+    opacity = opacity + 0.1;
+    body.style.opacity = opacity;
+  } else {
+    clearInterval(intervalID);
+  }
+}
+
+//night mode
+const dayTheme = function () {
+  mainBody.style.backgroundImage = "None";
+  mainBody.style.color = "black";
+  navBrandName.style.color = "black";
+  navBar.style.backgroundColor = "white";
+  nightModeIcon.src = "img/png/moon.png";
+  leetcode_color.setAttribute("fill", "#000");
+  marksIcon.forEach((e) => {
+    e.style.backgroundColor = "white";
+  });
+  projectCards.forEach((e) => {
+    e.style.backgroundColor = "white";
+  })
+  projectsContainer.style.backgroundColor = "#f4f4f4";
+};
+const nightTheme = function () {
+  mainBody.style.backgroundImage = "url(img/svg/background.svg)";
+  mainBody.style.color = "white";
+  navBrandName.style.color = "white";
+  navBar.style.backgroundColor = "#333";
+  nightModeIcon.src = "img/png/sun.png";
+  leetcode_color.setAttribute("fill", "#fff");
+  
+  marksIcon.forEach((e) => {
+    e.style.backgroundColor = "black";
+  });
+  projectCards.forEach((e) => {
+    e.style.backgroundColor = "black";
+  })
+  projectsContainer.style.backgroundColor = "#444";
+  // console.log(leetcode_color.fill);
+  // nightLabel.style.boxShadow=' 0rem 0rem 4rem 2rem rgba(251,251,0, 0.5)';
+  // nightLabel.style.backgroundColor='rgb(251,251,0)';
+};
+const changeTheme = function () {
+  if (nightModeBtn.checked === false) {
+    nightTheme();
+  } else {
+    dayTheme();
+  }
+  nightModeBtn.checked = !nightModeBtn.checked;
+};
+nightLabel.addEventListener("click", changeTheme);
 
 //nav-bar
-const switchColor = function(e) {
-
-    nav_items.forEach(function(ele) {
-        ele.style.color = '#fdbb04';
-    });
-    if (e === 'h') {
-        homeBtn.style.color = 'blue';
-    } else if (e == 'a') {
-        aboutBtn.style.color = 'blue';
-    } else if (e == 's') {
-        skillsBtn.style.color = 'blue';
-    } else if (e == 'p') {
-        projectsBtn.style.color = 'blue';
-    } else if (e == 'c') {
-        contactBtn.style.color = 'blue';
-    }
-}
-homeBtn.addEventListener('click', () => {
-    switchColor('h');
+const switchColor = function (e) {
+  nav_items.forEach(function (ele) {
+    ele.style.color = "#fdbb04";
+  });
+  if (e === "h") {
+    homeBtn.style.color = "gray";
+  } else if (e == "a") {
+    aboutBtn.style.color = "gray";
+  } else if (e == "s") {
+    skillsBtn.style.color = "gray";
+  } else if (e == "p") {
+    projectsBtn.style.color = "gray";
+  } else if (e == "c") {
+    contactBtn.style.color = "gray";
+  }
+};
+homeBtn.addEventListener("click", () => {
+  switchColor("h");
 });
-aboutBtn.addEventListener('click', () => {
-    switchColor('a');
+aboutBtn.addEventListener("click", () => {
+  switchColor("a");
 });
-skillsBtn.addEventListener('click', () => {
-    switchColor('s');
+skillsBtn.addEventListener("click", () => {
+  switchColor("s");
 });
-projectsBtn.addEventListener('click', () => {
-    switchColor('p');
+projectsBtn.addEventListener("click", () => {
+  switchColor("p");
 });
-contactBtn.addEventListener('click', () => {
-    switchColor('c');
+contactBtn.addEventListener("click", () => {
+  switchColor("c");
 });
 // section Home
-hireBtn.addEventListener('click', () => {
-        window.open('http://wa.me/919798447977');
-    })
-    //section About
-resumrBtn.addEventListener('click', () => {
-    window.open('https://drive.google.com/file/d/1ru9Hf8eroio2OrCo5Zq02Hn1xpYSN1sY/view?usp=sharing');
+hireBtn.addEventListener("click", () => {
+  window.open("http://wa.me/919798447977");
 });
-designerBtn.addEventListener('click', () => {
-    window.open('https://www.careerexplorer.com/careers/web-designer/');
+
+resumrBtn.addEventListener("click", () => {
+  window.open(
+    "https://drive.google.com/file/d/1YJcbr3HHzxBrUPSq8Xao3fuw6ZVlJD76/view?usp=sharing"
+  );
 });
-developerBtn.addEventListener('click', () => {
-    window.open('https://en.wikipedia.org/wiki/Web_developer');
+
+// section about (eduction)
+// section platform
+hacker_rank_btn.addEventListener("click", () => {
+  window.open("https://www.hackerrank.com/swas_k?hr_r=1");
 });
-projectManagerBtn.addEventListener('click', () => {
-    window.open('https://en.wikipedia.org/wiki/Project_manager');
+gfg_btn.addEventListener("click", () => {
+  window.open("https://auth.geeksforgeeks.org/user/swas_k/practice/");
 });
+leetcode_btn.addEventListener("click", () => {
+  window.open("https://leetcode.com/swas_k/");
+});
+codeforces_btn.addEventListener("click", () => {
+  window.open("https://codeforces.com/profile/swas_k");
+});
+codechef_btn.addEventListener("click", () => {
+  window.open("https://www.codechef.com/users/swas_k");
+});
+
+// section project
+
+cssProjectBtn.addEventListener("click", () => {
+  cssProjectBtn.classList.add("project__container__nav__btn--inactive")
+  jsProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  reactProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  backendProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  cppProjectBtn.classList.remove("project__container__nav__btn--inactive")
+
+  jsProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  reactProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  backendProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  cppProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  cssProjects.forEach((e) => {
+    e.style.removeProperty("display");
+  });
+});
+jsProjectBtn.addEventListener("click", () => {
+  jsProjectBtn.classList.add("project__container__nav__btn--inactive")
+  cssProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  reactProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  backendProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  cppProjectBtn.classList.remove("project__container__nav__btn--inactive")
+
+  jsProjects.forEach((e) => {
+    e.style.removeProperty("display");
+  });
+  reactProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  backendProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  cppProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  cssProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+});
+reactProjectBtn.addEventListener("click", () => {
+  cssProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  jsProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  reactProjectBtn.classList.add("project__container__nav__btn--inactive")
+  backendProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  cppProjectBtn.classList.remove("project__container__nav__btn--inactive")
+
+  jsProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  reactProjects.forEach((e) => {
+    e.style.removeProperty("display");
+  });
+  backendProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  cppProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  cssProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+});
+backendProjectBtn.addEventListener("click", () => {
+  cssProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  jsProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  reactProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  backendProjectBtn.classList.add("project__container__nav__btn--inactive")
+  cppProjectBtn.classList.remove("project__container__nav__btn--inactive")
+
+  jsProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  reactProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  backendProjects.forEach((e) => {
+    e.style.removeProperty("display");
+  });
+  cppProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  cssProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+});
+cppProjectBtn.addEventListener("click", () => {
+  cssProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  jsProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  reactProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  backendProjectBtn.classList.remove("project__container__nav__btn--inactive")
+  cppProjectBtn.classList.add("project__container__nav__btn--inactive")
+
+  jsProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  reactProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  backendProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+  cppProjects.forEach((e) => {
+    e.style.removeProperty("display");
+  });
+  cssProjects.forEach((e) => {
+    e.style.display = "none";
+  });
+});
+
+
+// for counting visitors
+function websiteVisits(response) {
+  document.querySelector("#visits").textContent = response.value;
+}
--- javascript/variables.js
+++ javascript/variables.js
@@ -1,4 +1,11 @@
+//night mode
+const nightModeBtn = document.getElementById('navi-toggle');
+const nightLabel = document.getElementById('night_label');
+const mainBody = document.getElementById('body');
+const nightModeIcon = document.querySelector('.night_mode_icon');
 //nav-bar
+const navBar = document.querySelector('.nav');
+const navBrandName = document.querySelector('.nav_brand-name');
 const nav_items = document.querySelectorAll('.nav_link')
 const homeBtn = document.querySelector('#home');
 const aboutBtn = document.querySelector('#about');
@@ -7,6 +14,31 @@ const projectsBtn = document.querySelector('#projects');
 const contactBtn = document.querySelector('#contact');
 //home Section
 const hireBtn = document.getElementById('hire');
+// eduction section
+const marksIcon = document.querySelectorAll('.marks__icon__circle__inner')
+// Project section
+const cssProjectBtn = document.getElementById('cssProjectButton');
+const jsProjectBtn = document.getElementById('jsProjectButton');
+const reactProjectBtn = document.getElementById('reactProjectButton');
+const backendProjectBtn = document.getElementById('backendProjectButton');
+const cppProjectBtn = document.getElementById('cppProjectButton');
+
+const cssProjects = document.querySelectorAll('.cssProject');
+const jsProjects = document.querySelectorAll('.jsProject');
+const reactProjects = document.querySelectorAll('.reactProject');
+const backendProjects = document.querySelectorAll('.backendProject');
+const cppProjects = document.querySelectorAll('.cppProject');
+
+const projectCards = document.querySelectorAll('.p__card');
+const projectsContainer = document.getElementById('projectContainer')
+
+// Platform Section
+const leetcode_color = document.getElementById('leetcode_path');
+const hacker_rank_btn = document.getElementById('hacker_rank_btn');
+const gfg_btn = document.getElementById('gfg_btn');
+const leetcode_btn = document.getElementById('leetcode_btn');
+const codeforces_btn = document.getElementById('codeforces_btn');
+const codechef_btn = document.getElementById('codechef_btn');
 //about section
 const resumrBtn = document.getElementById('resume');
 //about_section
