f7ad726bc48a0f5e9267101c846daf21a792038d
012SwastikKumar <swastiktovisit1234@gmail.com>
2023-07-10 00:51:24 +0530

    v1-basic-of-version_2

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 // Title tab
 window.onload = function () {
   let pageTitle = document.title;
-  let attentionMessage = "Come Back 🙏🙏";
+  let attentionMessage = "Come Back 🤓";
   let blinkEvent = null;
 
   document.addEventListener("visibilitychange", function (e) {
@@ -27,256 +27,106 @@ window.onload = function () {
   }
 };
 
-// fade in
-let opacity = 0;
-let intervalID = 0;
-let change_title = window.onload;
-window.onload = function () {
-  fadeIn();
-  if (change_title) {
-    change_title();
-  }
-};
-function fadeIn() {
-  setInterval(show, 110);
-}
-function show() {
-  var body = document.getElementById("body");
-  opacity = Number(window.getComputedStyle(body).getPropertyValue("opacity"));
-  if (opacity < 1) {
-    opacity = opacity + 0.1;
-    body.style.opacity = opacity;
+/**
+ * element toggle function
+ */
+
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+
+/**
+ * header sticky & go to top
+ */
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 10) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
   } else {
-    clearInterval(intervalID);
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
   }
+
+});
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+
+navToggleBtn.addEventListener("click", function () {
+
+  elemToggleFunc(navToggleBtn);
+  elemToggleFunc(navbar);
+  elemToggleFunc(document.body);
+
+});
+
+
+
+/**
+ * skills toggle
+ */
+
+const toggleBtnBox = document.querySelector("[data-toggle-box]");
+const toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+const skillsBox = document.querySelector("[data-skills-box]");
+
+for (let i = 0; i < toggleBtns.length; i++) {
+  toggleBtns[i].addEventListener("click", function () {
+
+    elemToggleFunc(toggleBtnBox);
+    for (let i = 0; i < toggleBtns.length; i++) { elemToggleFunc(toggleBtns[i]); }
+    elemToggleFunc(skillsBox);
+
+  });
 }
 
-//night mode
-const dayTheme = function () {
-  mainBody.style.backgroundImage = "None";
-  mainBody.style.color = "black";
-  navBrandName.style.color = "black";
-  navBar.style.backgroundColor = "white";
-  nightModeIcon.src = "img/png/moon.png";
-  leetcode_color.setAttribute("fill", "#000");
-  marksIcon.forEach((e) => {
-    e.style.backgroundColor = "white";
-  });
-  projectCards.forEach((e) => {
-    e.style.backgroundColor = "white";
-  })
-  projectsContainer.style.backgroundColor = "#f4f4f4";
-};
-const nightTheme = function () {
-  mainBody.style.backgroundImage = "url(img/svg/background.svg)";
-  mainBody.style.color = "white";
-  navBrandName.style.color = "white";
-  navBar.style.backgroundColor = "#333";
-  nightModeIcon.src = "img/png/sun.png";
-  leetcode_color.setAttribute("fill", "#fff");
-  
-  marksIcon.forEach((e) => {
-    e.style.backgroundColor = "black";
-  });
-  projectCards.forEach((e) => {
-    e.style.backgroundColor = "black";
-  })
-  projectsContainer.style.backgroundColor = "#444";
-  // console.log(leetcode_color.fill);
-  // nightLabel.style.boxShadow=' 0rem 0rem 4rem 2rem rgba(251,251,0, 0.5)';
-  // nightLabel.style.backgroundColor='rgb(251,251,0)';
-};
-const changeTheme = function () {
-  if (nightModeBtn.checked === false) {
-    nightTheme();
+
+
+/**
+ * dark & light theme toggle
+ */
+
+const themeToggleBtn = document.querySelector("[data-theme-btn]");
+
+themeToggleBtn.addEventListener("click", function () {
+
+  elemToggleFunc(themeToggleBtn);
+
+  if (themeToggleBtn.classList.contains("active")) {
+    document.body.classList.remove("dark_theme");
+    document.body.classList.add("light_theme");
+
+    localStorage.setItem("theme", "light_theme");
   } else {
-    dayTheme();
+    document.body.classList.add("dark_theme");
+    document.body.classList.remove("light_theme");
+
+    localStorage.setItem("theme", "dark_theme");
   }
-  nightModeBtn.checked = !nightModeBtn.checked;
-};
-nightLabel.addEventListener("click", changeTheme);
 
-//nav-bar
-const switchColor = function (e) {
-  nav_items.forEach(function (ele) {
-    ele.style.color = "#fdbb04";
-  });
-  if (e === "h") {
-    homeBtn.style.color = "gray";
-  } else if (e == "a") {
-    aboutBtn.style.color = "gray";
-  } else if (e == "s") {
-    skillsBtn.style.color = "gray";
-  } else if (e == "p") {
-    projectsBtn.style.color = "gray";
-  } else if (e == "c") {
-    contactBtn.style.color = "gray";
-  }
-};
-homeBtn.addEventListener("click", () => {
-  switchColor("h");
-});
-aboutBtn.addEventListener("click", () => {
-  switchColor("a");
-});
-skillsBtn.addEventListener("click", () => {
-  switchColor("s");
-});
-projectsBtn.addEventListener("click", () => {
-  switchColor("p");
-});
-contactBtn.addEventListener("click", () => {
-  switchColor("c");
-});
-// section Home
-hireBtn.addEventListener("click", () => {
-  window.open("http://wa.me/919798447977");
 });
 
-resumrBtn.addEventListener("click", () => {
-  window.open(
-    "https://drive.google.com/file/d/1oTcfkNWlKaiTl8YkBZeWQiYIC61s8lRK/view?usp=sharing"
-  );
-});
+/**
+ * check & apply last time selected theme from localStorage
+ */
 
-// section about (eduction)
-// section platform
-hacker_rank_btn.addEventListener("click", () => {
-  window.open("https://www.hackerrank.com/swas_k?hr_r=1");
-});
-gfg_btn.addEventListener("click", () => {
-  window.open("https://auth.geeksforgeeks.org/user/swas_k/practice/");
-});
-leetcode_btn.addEventListener("click", () => {
-  window.open("https://leetcode.com/swas_k/");
-});
-codeforces_btn.addEventListener("click", () => {
-  window.open("https://codeforces.com/profile/swas_k");
-});
-codechef_btn.addEventListener("click", () => {
-  window.open("https://www.codechef.com/users/swas_k");
-});
-
-// section project
-
-cssProjectBtn.addEventListener("click", () => {
-  cssProjectBtn.classList.add("project__container__nav__btn--inactive")
-  jsProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  reactProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  backendProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  cppProjectBtn.classList.remove("project__container__nav__btn--inactive")
-
-  jsProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  reactProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  backendProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  cppProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  cssProjects.forEach((e) => {
-    e.style.removeProperty("display");
-  });
-});
-jsProjectBtn.addEventListener("click", () => {
-  jsProjectBtn.classList.add("project__container__nav__btn--inactive")
-  cssProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  reactProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  backendProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  cppProjectBtn.classList.remove("project__container__nav__btn--inactive")
-
-  jsProjects.forEach((e) => {
-    e.style.removeProperty("display");
-  });
-  reactProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  backendProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  cppProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  cssProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-});
-reactProjectBtn.addEventListener("click", () => {
-  cssProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  jsProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  reactProjectBtn.classList.add("project__container__nav__btn--inactive")
-  backendProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  cppProjectBtn.classList.remove("project__container__nav__btn--inactive")
-
-  jsProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  reactProjects.forEach((e) => {
-    e.style.removeProperty("display");
-  });
-  backendProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  cppProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  cssProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-});
-backendProjectBtn.addEventListener("click", () => {
-  cssProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  jsProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  reactProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  backendProjectBtn.classList.add("project__container__nav__btn--inactive")
-  cppProjectBtn.classList.remove("project__container__nav__btn--inactive")
-
-  jsProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  reactProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  backendProjects.forEach((e) => {
-    e.style.removeProperty("display");
-  });
-  cppProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  cssProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-});
-cppProjectBtn.addEventListener("click", () => {
-  cssProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  jsProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  reactProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  backendProjectBtn.classList.remove("project__container__nav__btn--inactive")
-  cppProjectBtn.classList.add("project__container__nav__btn--inactive")
-
-  jsProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  reactProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  backendProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-  cppProjects.forEach((e) => {
-    e.style.removeProperty("display");
-  });
-  cssProjects.forEach((e) => {
-    e.style.display = "none";
-  });
-});
-
-
-// for counting visitors
-function websiteVisits(response) {
-  document.querySelector("#visits").textContent = response.value;
+if (localStorage.getItem("theme") === "light_theme") {
+  themeToggleBtn.classList.add("active");
+  document.body.classList.remove("dark_theme");
+  document.body.classList.add("light_theme");
+} else {
+  themeToggleBtn.classList.remove("active");
+  document.body.classList.remove("light_theme");
+  document.body.classList.add("dark_theme");
 }
